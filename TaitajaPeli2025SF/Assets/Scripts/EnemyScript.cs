5baea6365a6e14fe11e633cf94cacef7b5aedac2
Lucash5 <122258329+Lucash5@users.noreply.github.com>
2025-01-30 10:38:07 +0200

    aaa

---
+++ TaitajaPeli2025SF/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float bulletDamageVulnerability;
+
+    bool attackCooldown = false;
+    public float attackCooldownDuration;
+    public float health;
+
+    public float attackDamage;
+    public float movementSpeed;
+    public Transform player;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = player.position - transform.position;
+
+        float XYSum = Mathf.Abs( direction.x) + Mathf.Abs(direction.y);
+        float XInfluence = direction.x / XYSum; 
+        float YInfluence = direction.y / XYSum; 
+
+        rb.velocity = new Vector2(XInfluence * movementSpeed, YInfluence * movementSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player" && !attackCooldown)
+        {
+            attackCooldown = true;
+            StartCoroutine(inflictDamage(collision.gameObject));
+            
+        }
+    }
+
+    IEnumerator inflictDamage(GameObject player)
+    {
+
+        player.GetComponent<PlayerStats>().TakeDamage(attackDamage);
+        yield return new WaitForSeconds(attackCooldownDuration);
+        attackCooldown = false;
+    }
+
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Bullet")
+        {
+            takeDamage(bulletDamageVulnerability);
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
---
+++ TaitajaPeli2025SF/Assets/Scripts/PlayerWeaponScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponScript : MonoBehaviour
+{
+    bool gunCooldown;
+    public float fireRate;
+    public float bulletVelocity;
+    public Transform firingPoint;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && gunCooldown == false)
+        {
+            StartCoroutine(FireBullet());
+        }
+    }
+   IEnumerator FireBullet()
+    {
+        gunCooldown = true;
+        GameObject instantiatedBullet = Instantiate(bullet);
+        instantiatedBullet.transform.position = firingPoint.position;
+        instantiatedBullet.transform.rotation = firingPoint.rotation;   
+        instantiatedBullet.GetComponent<Rigidbody2D>().AddForce(transform.forward * bulletVelocity);
+
+        yield return new WaitForSeconds(fireRate);
+        gunCooldown = false;
+    }
+
+}
